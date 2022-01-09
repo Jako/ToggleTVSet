380bb3c20e68a3f36d49bec786e24e2a85cac8b0
Thomas Jakobi <thomas.jakobi@partout.info>
2022-01-09 01:41:22 +0100

    Normalize code

--- core/components/toggletvset/model/toggletvset/toggletvset.class.php
+++ core/components/toggletvset/model/toggletvset/toggletvset.class.php
@@ -164,8 +164,8 @@ class ToggleTVSet
                 $option = $options[$key];
             } elseif (array_key_exists($key, $this->options)) {
                 $option = $this->options[$key];
-            } elseif (array_key_exists("{$this->namespace}.{$key}", $this->modx->config)) {
-                $option = $this->modx->getOption("{$this->namespace}.{$key}");
+            } elseif (array_key_exists("$this->namespace.$key", $this->modx->config)) {
+                $option = $this->modx->getOption("$this->namespace.$key");
             }
         }
         return $option;
