46cbfc87b38908d06e8cf32b8f84bc260368d393
Thomas Jakobi <thomas.jakobi@partout.info>
2025-03-09 17:34:19 +0100

    Fix a regular expression detecting the version in the main class

--- gulpfile.js
+++ gulpfile.js
@@ -93,7 +93,7 @@ const bumpVersion = function () {
     return gulp.src([
         'core/components/toggletvset/src/ToggleTVSet.php'
     ], {base: './'})
-        .pipe(replace(/version = '\d+\.\d+\.\d+[-a-z0-9]*'/ig, 'version = \'' + pkg.version + '\''))
+        .pipe(replace(/version = '\d+\.\d+\.\d+-?[0-9a-z]*'/ig, 'version = \'' + pkg.version + '\''))
         .pipe(gulp.dest('.'));
 };
 const bumpHomepanel = function () {
